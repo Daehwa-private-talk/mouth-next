4ed60b833c817f70a99f805a8fffeea894caf539
kimnamsun <nsunny0908@naver.com>
2023-12-06 21:30:04 +0900

    style: primary color, secondary color 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,17 @@
+type ColorVariant = 'main' | 'sub';
+
+export type PrimaryColorType = { [variant in ColorVariant]: string };
+
 export type Color = 'black' | 'dark' | 'violet' | 'purple' | 'red' | 'yellow';
 
+const primaryColors: PrimaryColorType = {
+  main: '#ffee93',
+  sub: '#fcf5c7',
+};
+
 export const colors = {
+  primary: primaryColors,
+  secondary: '#ffc09f',
   white: '#ffffff',
   black: '#333333',
   dark: '#183f3c',
