ee9a4aa3aafb11e407578d8ff93e5ea18b5e0056
leejiwon6315 <leejiwon6315@naver.com>
2023-12-17 18:59:55 +0900

    refactor: auth wrapper 조건 간결화

--- src/components/wrappers/AuthWrapper.tsx
+++ src/components/wrappers/AuthWrapper.tsx
@@ -5,7 +5,7 @@ import { ACCESS_TOKEN_TITLE, IS_USER } from '@/constants/common';
 import { SIGN_IN_PATH, SIGN_UP_PATH } from '@/constants/path/auth';
 import { LIST } from '@/constants/path/chat';
 import { useAuthMe } from '@/hooks/auth/useAuthMe';
-import { getCookie, deleteCookie } from 'cookies-next';
+import { deleteCookie, getCookie } from 'cookies-next';
 import { useEffect } from 'react';
 
 const PUBLIC_ROUTES = [SIGN_IN_PATH, SIGN_UP_PATH];
@@ -26,21 +26,15 @@ export default function AuthWrapper({ children }: StrictPropsWithChildren) {
 
       if (error || !accessToken) {
         removeAllCookies();
-
-        if (error) {
-          window.location.assign(SIGN_IN_PATH);
-          return;
-        }
       }
 
-      if (ROOT.includes(currentPath)) {
+      if (error || ROOT.includes(currentPath)) {
         window.location.assign(SIGN_IN_PATH);
         return;
       }
 
       if (PUBLIC_ROUTES.includes(currentPath)) {
         if (!accessToken) {
-          removeAllCookies();
           return;
         }
 
